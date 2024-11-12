cb1a235c01afa8ed93d87fff0603747cd2d863b8
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2024-11-12 18:57:16 +0200

    second try

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -7,19 +7,44 @@ function makeCalculator() {
   let result = 0;
 
   return {
-    add: (num) => result + num,
+    add(num) {
+      result += num;
 
-    subtract: (num) => result - num,
+      return result;
+    },
 
-    multiply: (num) => result * num,
+    subtract(num) {
+      result -= num;
 
-    divide: (num) => (num !== 0 ? result / num : NaN),
+      return result;
+    },
+
+    multiply(num) {
+      result *= num;
+
+      return result;
+    },
+
+    divide(num) {
+      if (num !== 0) {
+        result /= num;
+      } else {
+        result = NaN;
+      }
+
+      return result;
+    },
 
     operate(callback, num) {
-      result = callback(num);
+      if (typeof callback === 'function') {
+        result = callback(num);
+      } else {
+        throw new Error('The provided callback is not a function');
+      }
 
       return this;
     },
+
     reset() {
       result = 0;
 
